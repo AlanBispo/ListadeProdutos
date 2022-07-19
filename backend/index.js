e57656d85a04f7815e8ea409f320257485cac618
Alandelon Bispo <alanbsts50@gmail.com>
2022-07-18 21:50:01 -0300

    back-end

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
-const produtos = [{}]
+
+let produtos = []
 
 //Retorna todos os produtos
 server.get('/produtos', (req, res) => {
@@ -15,34 +17,54 @@ server.get('/produtos', (req, res) => {
 //Retorna um produto
 server.get('/produtos/:index', (req, res) => {
     const { index } = req.params
+    let produtosAux;
 
-    return res.json(produtos[index])
+    produtosAux = produtos.filter(item => item.id == index);
+
+    return res.json(produtosAux)
 })
 
 //Criar novo produto
 server.post('/produtos', (req, res) => {
-    const { name } = req.body
-    const { cost } = req.body
-    
-    produtos.push(name, cost)
-    
+    let produto = req.body;
+    let gerarId;
+
+    if (produtos.length > 0) {
+        gerarId = [...produtos].pop();
+        console.log("id: ", gerarId)
+        gerarId = gerarId.id + 1;
+    } else {
+        gerarId = 1;
+    }
+
+    produto.id = gerarId;
+
+    produtos.push(produto)
+
     return res.json(produtos)
 })
 
 //Atualiza um produto
-server.put('/produtos/:index', (req, res) => {
-    const { index } = req.params
-    const { name } = req.body
+server.put('/produtos', (req, res) => {
 
-    produtos[index] = name
+    let produto = req.body;
+
+    produtos.map(item => {
+        if (item.id == produto.id) {
+            item.nome = produto.nome;
+            item.valor = produto.valor;
+            item.qtd = produto.qtd;
+        }
+    })
 
     return res.json(produtos)
 })
+
 //Deleta um produto 
 server.delete('/produtos/:index', (req, res) => {
     const { index } = req.params
-    
-    produtos.splice(index, 1)
+
+    produtos = produtos.filter(item => item.id != index);
 
     return res.json({ message: 'O curso foi deletado' })
 })
